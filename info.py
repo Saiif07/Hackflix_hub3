e0f301ce492f1cce06ca10142edbd89929061605
Saif <311499990+Saiif07@users.noreply.github.com>
2026-09-03 17:05:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'hackflix') # Collection name (
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://sahnwaja109_db_user:<db_password>@cluster2.kujztjv.mongodb.net/?appName=Cluster2")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_NAME = environ.get('DATABASE_NAME', "hackflix_01") # Database name (default: cluster)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'hackflix_01') # Collection name (default: dreamcinezone_files
 # ============================
 # Movie Notification & Update Settings
 # ============================
